ebc36b031902b04182e4b925c59ee813f4b3b27a
zalayetha <zalayetha@gmail.com>
2026-09-13 10:22:12 +0700

    chore: run prisma emit contract

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,10 +1,9 @@
-// import postgres from "@prisma/orm-postgres/runtime";
-// import type { Contract } from "../generated/prisma/contract";
-// import { contractJson } from "../generated/prisma/contract";
+import "dotenv/config";
+import postgres from "@prisma/orm-postgres/runtime";
+import type { Contract } from "../generated/prisma/contract";
+import contractJson from "../generated/prisma/contract.json" with { type: "json" };
 
-// export const db = postgres<Contract>(
-//   {
-//     contractJson,
-//     url: process.env.DATABASE_URL!,
-//   }
-// )
+export const db = postgres<Contract>({
+  contractJson,
+  url: process.env["DATABASE_URL"]!,
+});
