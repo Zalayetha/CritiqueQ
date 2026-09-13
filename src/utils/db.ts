3c45d606d799ce5428767842f3037a2b00b494a5
zalayetha <zalayetha@gmail.com>
2026-09-13 09:55:58 +0700

    initial commit

---
+++ src/utils/db.ts
@@ -0,0 +1,10 @@
+// import postgres from "@prisma/orm-postgres/runtime";
+// import type { Contract } from "../generated/prisma/contract";
+// import { contractJson } from "../generated/prisma/contract";
+
+// export const db = postgres<Contract>(
+//   {
+//     contractJson,
+//     url: process.env.DATABASE_URL!,
+//   }
+// )
